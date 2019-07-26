8fd4157773e62c785525ea9b30b5d12cd1c04d3f
sakda homhuan <sakda.homhuan@gmail.com>
2019-07-26 21:36:10 +0700

    add hotspot

--- hotspot.js
+++ hotspot.js
@@ -5,6 +5,9 @@ const request = require('request');
 const csv = require('csvtojson');
 const turf = require('@turf/turf');
 
+// const http = require('http');
+// const request = require('request');
+
 //concr con = require('./conn');
 //concr db = con.th;
 
@@ -18,12 +21,15 @@ const db = new Pool({
 });
 
 const json = require('./pv4');
-
+const proJson = require('./geojson');
+// console.log(proJson);
 // ph nn py cr extent
 var poly_ph = turf.polygon(json.ph.features[0].geometry.coordinates[0]);
 var poly_py = turf.polygon(json.py.features[0].geometry.coordinates[0]);
 var poly_nn = turf.polygon(json.nn.features[0].geometry.coordinates[0]);
 var poly_cr = turf.polygon(json.cr.features[0].geometry.coordinates[0]);
+var pro = turf.polygon(proJson.pro.features[0].geometry.coordinates[0]);
+// console.log(json.cr.features[0].geometry.coordinates[0])
 var poly = turf.polygon([
     [
         [99.36734051792395, 16.320423380302735],
@@ -34,21 +40,75 @@ var poly = turf.polygon([
     ]
 ]);
 
+router.get("/hp_test", async (req, res, next) => {
+    // const lat = req.params.lat;
+    // const lon = req.params.lon;
+    const urlServer = 'http://119.59.125.191/geolab/hotspot3.csv';
+    const urlFirms = 'https://firms.modaps.eosdis.nasa.gov/active_fire/c6/text/MODIS_C6_SouthEast_Asia_7d.csv';
+    csv().fromStream(request.get(urlServer)).then(async (data) => {
+        let jsonFeatures = [];
+        let ph = 0;
+        let py = 0;
+        let nn = 0;
+        let cr = 0;
+        data.forEach((point) => {
+            let lat = Number(point.latitude);
+            let lon = Number(point.longitude);
+            // console.log(point);
+            let pt = turf.point([lon, lat]);
+            if (turf.booleanPointInPolygon(pt, pro) == true) {
+                // console.log(pt)
+                const url = `http://119.59.125.191/geoserver/omfs/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=omfs:tambon&outputFormat=application%2Fjson&CQL_FILTER=INTERSECTS(geom,Point(${lon}%20${lat}))`;
+                // let feature;
+                request(url, {
+                    json: true
+                }, (err, res, body) => {
+                    point.tam = body.features[0].properties;
+                });
+
+                let feature = {
+                    type: 'Feature',
+                    properties: point,
+                    geometry: {
+                        type: 'Point',
+                        coordinates: [lon, lat]
+                    }
+                };
+                jsonFeatures.push(feature);
+            };
+        })
+        let geoJson = {
+            type: 'FeatureCollection',
+            features: jsonFeatures
+        };
+        res.status(200).json({
+            cratus: 'success',
+            ph: ph,
+            py: py,
+            nn: nn,
+            cr: cr,
+            data: geoJson,
+            message: 'retrived survey data'
+        })
+    }).catch((error) => {
+        return next(error)
+    })
+})
 
 router.get("/hp_modis", async function (req, res, next) {
     csv().fromStream(request.get('https://firms.modaps.eosdis.nasa.gov/active_fire/c6/text/MODIS_C6_SouthEast_Asia_7d.csv'))
-  // csv().fromStream(request.get('https://firms.modaps.eosdis.nasa.gov/active_fire/c6/text/MODIS_C6_SouthEast_Asia_7d.csv'))
+        // csv().fromStream(request.get('https://firms.modaps.eosdis.nasa.gov/active_fire/c6/text/MODIS_C6_SouthEast_Asia_7d.csv'))
         .then(async (data) => {
-            
+
             let jsonFeatures = [];
             let ph = 0;
             let py = 0;
             let nn = 0;
             let cr = 0;
-            data.forEach((point)=> {
+            data.forEach((point) => {
                 // let lat = Number(point.latitnne);
                 // let lon = Number(point.longitnne);
-               //console.log(data);  
+                //console.log(data);  
                 let lat = Number(point.latitude);
                 let lon = Number(point.longitude);
                 let pt = turf.point([lon, lat]);
@@ -57,7 +117,7 @@ router.get("/hp_modis", async function (req, res, next) {
                 if (turf.booleanPointInPolygon(pt, poly_nn) == true) nn += 1;
                 if (turf.booleanPointInPolygon(pt, poly_cr) == true) cr += 1;
                 if (turf.booleanPointInPolygon(pt, poly) == true) {
-                    
+
                     let feature = {
                         type: 'Feature',
                         properties: point,
@@ -71,18 +131,18 @@ router.get("/hp_modis", async function (req, res, next) {
                     const sql = {
                         text: 'INSERT INTO hp_modis(latitude, longitude, brightness, scan, track, acq_date, acq_time, satellite, instrument, confidence, version, bright_t31, frp, daynight) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)',
                         //values: [point.latitude, point.longitude, point.brightness, point.scan, point.track, point.acq_date, point.acq_time, point.satellite, point.instrument, point.confidence, point.version, point.bright_t31, point.frp, point.daynight ],
-                        values: [point.latitude, point.longitude, point.brightness, point.scan, point.track, point.acq_date, point.acq_time, point.satellite, point.instrument, point.confidence, point.version, point.bright_t31, point.frp, point.daynight ],
-                      }
-                       db.query(sql)
+                        values: [point.latitude, point.longitude, point.brightness, point.scan, point.track, point.acq_date, point.acq_time, point.satellite, point.instrument, point.confidence, point.version, point.bright_t31, point.frp, point.daynight],
+                    }
+                    db.query(sql)
 
                 }
-               //const sql = `INSERT INTO hp_modis (latitude, longitude, brightness, scan, track, acq_date, acq_time, satellite, instrument, confidence, version, bright_t31, frp) VALUES (data.latitude, data.longitude, data.brightness, data.scan, data.track, data.acq_date, 'data.acq_time', 'data.satellite', 'data.instrument', 'data.confidence', 'data.version', data.bright_t31, data.frp)`;
-               //var user = req.body;
-               //const sql = {
-               // text: 'INSERT INTO hp_modis(latitude, longitude, brightness, scan, track, acq_date, acq_time, satellite, instrument, confidence, version, bright_t31, frp, daynight) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)',
+                //const sql = `INSERT INTO hp_modis (latitude, longitude, brightness, scan, track, acq_date, acq_time, satellite, instrument, confidence, version, bright_t31, frp) VALUES (data.latitude, data.longitude, data.brightness, data.scan, data.track, data.acq_date, 'data.acq_time', 'data.satellite', 'data.instrument', 'data.confidence', 'data.version', data.bright_t31, data.frp)`;
+                //var user = req.body;
+                //const sql = {
+                // text: 'INSERT INTO hp_modis(latitude, longitude, brightness, scan, track, acq_date, acq_time, satellite, instrument, confidence, version, bright_t31, frp, daynight) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)',
                 //values: [point.latitude, point.longitude, point.brightness, point.scan, point.track, point.acq_date, point.acq_time, point.satellite, point.instrument, point.confidence, point.version, point.bright_t31, point.frp, point.daynight ],   
-              //}
-              // db.query(sql)
+                //}
+                // db.query(sql)
 
             });
             let geoJson = {
@@ -105,14 +165,14 @@ router.get("/hp_modis", async function (req, res, next) {
 
 router.get("/hp_modis24", async function (req, res, next) {
     //csv().fromStream(request.get('http://119.59.125.191/geolab/hotspot3.csv'))
-   csv().fromStream(request.get('https://firms.modaps.eosdis.nasa.gov/active_fire/c6/text/MODIS_C6_SouthEast_Asia_24h.csv'))
+    csv().fromStream(request.get('https://firms.modaps.eosdis.nasa.gov/active_fire/c6/text/MODIS_C6_SouthEast_Asia_24h.csv'))
         .then(async (data) => {
             let jsonFeatures = [];
             let ph = 0;
             let py = 0;
             let nn = 0;
             let cr = 0;
-            data.forEach((point)=> {
+            data.forEach((point) => {
 
                 let lat = Number(point.latitude);
                 let lon = Number(point.longitude);
@@ -122,7 +182,7 @@ router.get("/hp_modis24", async function (req, res, next) {
                 if (turf.booleanPointInPolygon(pt, poly_nn) == true) nn += 1;
                 if (turf.booleanPointInPolygon(pt, poly_cr) == true) cr += 1;
                 if (turf.booleanPointInPolygon(pt, poly) == true) {
-                    
+
                     let feature = {
                         type: 'Feature',
                         properties: point,
@@ -156,15 +216,15 @@ router.get("/hp_modis24", async function (req, res, next) {
 
 router.get("/hp_modis48", async function (req, res, next) {
     csv().fromStream(request.get('https://firms.modaps.eosdis.nasa.gov/active_fire/c6/text/MODIS_C6_SouthEast_Asia_48h.csv'))
-  // csv().fromStream(request.get('https://firms.modaps.eosdis.nasa.gov/active_fire/c6/text/MODIS_C6_SouthEast_Asia_7d.csv'))
+        // csv().fromStream(request.get('https://firms.modaps.eosdis.nasa.gov/active_fire/c6/text/MODIS_C6_SouthEast_Asia_7d.csv'))
         .then(async (data) => {
-            
+
             let jsonFeatures = [];
             let ph = 0;
             let py = 0;
             let nn = 0;
             let cr = 0;
-            data.forEach((point)=> { 
+            data.forEach((point) => {
                 let lat = Number(point.latitude);
                 let lon = Number(point.longitude);
                 let pt = turf.point([lon, lat]);
@@ -173,7 +233,7 @@ router.get("/hp_modis48", async function (req, res, next) {
                 if (turf.booleanPointInPolygon(pt, poly_nn) == true) nn += 1;
                 if (turf.booleanPointInPolygon(pt, poly_cr) == true) cr += 1;
                 if (turf.booleanPointInPolygon(pt, poly) == true) {
-                    
+
                     let feature = {
                         type: 'Feature',
                         properties: point,
@@ -206,15 +266,15 @@ router.get("/hp_modis48", async function (req, res, next) {
 
 router.get("/hp_modis7", async function (req, res, next) {
     csv().fromStream(request.get('https://firms.modaps.eosdis.nasa.gov/active_fire/c6/text/MODIS_C6_SouthEast_Asia_7d.csv'))
-  // csv().fromStream(request.get('https://firms.modaps.eosdis.nasa.gov/active_fire/c6/text/MODIS_C6_SouthEast_Asia_7d.csv'))
+        // csv().fromStream(request.get('https://firms.modaps.eosdis.nasa.gov/active_fire/c6/text/MODIS_C6_SouthEast_Asia_7d.csv'))
         .then(async (data) => {
-            
+
             let jsonFeatures = [];
             let ph = 0;
             let py = 0;
             let nn = 0;
             let cr = 0;
-            data.forEach((point)=> {
+            data.forEach((point) => {
                 let lat = Number(point.latitude);
                 let lon = Number(point.longitude);
                 let pt = turf.point([lon, lat]);
@@ -223,7 +283,7 @@ router.get("/hp_modis7", async function (req, res, next) {
                 if (turf.booleanPointInPolygon(pt, poly_nn) == true) nn += 1;
                 if (turf.booleanPointInPolygon(pt, poly_cr) == true) cr += 1;
                 if (turf.booleanPointInPolygon(pt, poly) == true) {
-                    
+
                     let feature = {
                         type: 'Feature',
                         properties: point,
@@ -233,7 +293,7 @@ router.get("/hp_modis7", async function (req, res, next) {
                         }
                     };
                     jsonFeatures.push(feature);
-                    
+
                 }
 
             });
@@ -273,7 +333,7 @@ router.get("/hp_viirs", async function (req, res, next) {
                 if (turf.booleanPointInPolygon(pt, poly_cr) == true) cr += 1;
                 if (turf.booleanPointInPolygon(pt, poly) == true) {
 
-                   
+
                     let feature = {
                         type: 'Feature',
                         properties: point,
@@ -287,9 +347,9 @@ router.get("/hp_viirs", async function (req, res, next) {
 
                     const sql = {
                         text: 'INSERT INTO hp_modis(latitude, longitude, bright_ti4, scan, track, acq_date, acq_time, satellite, instrument, confidence, version, bright_ti5, frp, daynight) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)',
-                        values: [point.latitude, point.longitude, point.bright_ti4, point.scan, point.track, point.acq_date, point.acq_time, point.satellite, point.instrument, point.confidence, point.version, point.bright_ti5, point.frp, point.daynight ],
-                      }
-                       db.query(sql)
+                        values: [point.latitude, point.longitude, point.bright_ti4, point.scan, point.track, point.acq_date, point.acq_time, point.satellite, point.instrument, point.confidence, point.version, point.bright_ti5, point.frp, point.daynight],
+                    }
+                    db.query(sql)
 
                 }
 
@@ -331,7 +391,7 @@ router.get("/hp_viirs24", async function (req, res, next) {
                 if (turf.booleanPointInPolygon(pt, poly_cr) == true) cr += 1;
                 if (turf.booleanPointInPolygon(pt, poly) == true) {
 
-                   
+
                     let feature = {
                         type: 'Feature',
                         properties: point,
@@ -382,7 +442,7 @@ router.get("/hp_viirs48", async function (req, res, next) {
                 if (turf.booleanPointInPolygon(pt, poly_cr) == true) cr += 1;
                 if (turf.booleanPointInPolygon(pt, poly) == true) {
 
-                   
+
                     let feature = {
                         type: 'Feature',
                         properties: point,
@@ -433,7 +493,7 @@ router.get("/hp_viirs7", async function (req, res, next) {
                 if (turf.booleanPointInPolygon(pt, poly_cr) == true) cr += 1;
                 if (turf.booleanPointInPolygon(pt, poly) == true) {
 
-                   
+
                     let feature = {
                         type: 'Feature',
                         properties: point,
