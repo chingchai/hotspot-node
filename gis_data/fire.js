52b7a17e7b48e3690e3a1a708ca2ae6ff11cd93e
Chingchai Humhong <chingchai.h@gmail.com>
2019-07-27 00:16:45 +0700

    update turf

--- gis_data/fire.js
+++ gis_data/fire.js
@@ -1,12 +1,5 @@
 var fire = {
     "type": "FeatureCollection",
-    "name": "fp",
-    "crs": {
-        "type": "name",
-        "properties": {
-            "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
-        }
-    },
     "features": [{
             "type": "Feature",
             "properties": {
